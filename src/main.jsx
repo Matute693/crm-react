f8c9151d93f809d2e878b839eb9d1c8f9922cc81
Matias Nicolas Michelli <matias.michelli@naranja.com>
2022-10-25 09:33:49 -0300

    install libraries

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import './index.css'
-
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Layout from './components/Layout';
+import NuevoCliente from './pages/NuevoCliente'
+import Index from './pages/Index'
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout/>,
+    children: [
+      {
+        index: true, //Renderiza la primer vista que quiero renderizar
+        element: <Index />
+      },
+      {
+        path: '/clientes/nuevo',
+        element: <NuevoCliente />
+      }
+    ]
+  },
+  {
+    path: '/clientes/nuevo',
+    element: <h1>Nosotros</h1>
+  }
+])
+ 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider  router={ router }/>
   </React.StrictMode>
 )
